efd5d4316670428ecb40947b9fc378a1a3e60c39
piccori <taisei.higa.1999@icloud.com>
2021-06-26 21:08:46 +0900

    JSX

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,19 @@
+import React from "react";
+
 function App() {
+	const hi = <h1 className="foo">Hi!</h1>;
+	const greeting = "Hi, Ken!";
 	return (
 		<div>
-			<h1>Hello,World!</h1>
+			<h1>{hi}World!</h1>
+			<h1>{greeting}</h1>
+			<label htmlFor="foo">Test</label>
+			<input
+				type="text"
+				onClick={() => {
+					console.log("Hello");
+				}}
+			/>
 		</div>
 	);
 }
