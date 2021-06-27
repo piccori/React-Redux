fe705597e9f5de357a7a4a009ec736045f973abe
piccori <taisei.higa.1999@icloud.com>
2021-06-27 20:53:45 +0900

    state

--- react-tutorial/src/js/components/Layout.js
+++ react-tutorial/src/js/components/Layout.js
@@ -3,9 +3,17 @@ import Header from "./Header";
 import Footer from "./Footer";
 
 export default class Layout extends React.Component {
+	constructor() {
+		super();
+		this.state = { name: "Piccori" };
+	}
 	render() {
+		setTimeout(() => {
+			this.setState({ name: "Hello" });
+		}, 1000);
 		return (
 			<div>
+				{this.state.name}
 				<Header />
 				<Footer />
 			</div>
